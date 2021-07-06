8a7632505b549942a3f9e5a8454e37855abd75e9
Rick Fletcher <fletch@pobox.com>
2021-07-06 13:23:23 -0400

    Fix cli-privacy 0.0.2 checksum

--- Formula/cli-privacy.rb
+++ Formula/cli-privacy.rb
@@ -2,7 +2,7 @@ class CliPrivacy < Formula
   homepage "https://github.com/rfletcher/cli-privacy"
   url      "https://github.com/rfletcher/cli-privacy/archive/refs/tags/0.0.2.tar.gz"
   version  "0.0.2"
-  sha256   "71614a67db67bcb487f2642f32054fe575b72509a0f080dabb329b50a8eaa7ca"
+  sha256   "908b42b124f309ffa8c8ab7b9314cc2db5ba57652499f911c531c012380bfe25"
 
   def install
     bin.install_symlink "../cli-privacy"
